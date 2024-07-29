28f911ed41fe83cab056056b74bff7a6a72945d3
Patrick Vink <patrick.vink@ista.nl>
2024-07-29 02:09:37 +0200

    todo Sorting

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
-﻿WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer();
+﻿using System.Text.RegularExpressions;
+
+WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer();
+WMPLib.IWMPMedia clip;
 string path = args.Length > 0 ? args[0] : Environment.CurrentDirectory;
 if (path.Last() == '\\')
     path = path.Substring(0, path.Length - 1);
 string[] files = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+Sort();
 List<string> m3u = [];
 Add("#EXTM3U");
 foreach (var file in files)
@@ -15,24 +19,48 @@ foreach (var file in files)
             break;
     }
 }
-if (m3u.Count > 1)
+if (HasItems())
 {
     path = Path.Combine(path, "playlist.m3u");
-    File.WriteAllLines(path, m3u);
+    //File.WriteAllLines(path, m3u);
 }
+void Sort()
+{
+    return;
+
+    var filtered = files
+        .Where(HasValidExtension);
+    var matches = filtered
+        .Select(x => (NumericExtract().Match(Path.GetFileName(x)), x));
+    var keyed = matches
+        .Select(x => (int.Parse(x.Item1.Groups.Values.ToList()[1].Value), int.Parse(x.Item1.Groups.Values.ToList()[2].Value), x.Item2));
+    var indexed = keyed
+        .OrderBy(x => x.Item1).ThenBy(x => x.Item2);
+    var sorted = indexed
+        .Select(x => x.Item3);
+    files = sorted
+        .ToArray();
+}
+bool HasValidExtension(string path) => Path.GetExtension(path).ToLower() switch 
+{ 
+    ".mp4" => true, 
+    _ => false 
+};
 void Add(string file)
 {
-    if (file.StartsWith('#'))
+    if (file.Equals("#EXTM3U"))
     {
         InternalAdd(file);
         return;
     }
     //FileInfo fileInfo = new FileInfo(file);
+    clip = player.newMedia(file);
     int duration = GetDuration(file);
     file = file.Substring(path.Length + 1);
     string title = Path.ChangeExtension(Path.GetFileName(file), null);
     InternalAdd($"#EXTINF:{duration},{title}");
     InternalAdd(file);
+    player.close();
 }
 void InternalAdd(string s)
 {
@@ -40,9 +68,14 @@ void InternalAdd(string s)
     m3u.Add(s);
 }
 int GetDuration(string file)
-{
-    var clip = player.newMedia(file);
+{   
     var duration = Convert.ToInt32(TimeSpan.FromSeconds(clip.duration).TotalSeconds);
-    player.close();
     return duration;
+}
+bool HasItems() => m3u.Count > 1;
+
+partial class Program
+{
+    [GeneratedRegex(@"(\d+).+(\d+).")]
+    private static partial Regex NumericExtract();
 }
